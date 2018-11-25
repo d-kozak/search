b44e6608c97ea3d7aa350419b277dd9be0c3abda
dkozak <dkozak94@gmail.com>
2018-11-25 14:47:00 +0100

    separate static and dynamic part of sokoban state

--- src/main/kotlin/io/dkozak/search/astar/sokoban/map.kt
+++ src/main/kotlin/io/dkozak/search/astar/sokoban/map.kt
@@ -2,9 +2,9 @@ package io.dkozak.search.astar.sokoban
 
 typealias Location = Pair<Int, Int>
 
-sealed class Field {
-    object Wall : Field()
-    class Path(val isDiamondGoal: Boolean, var hasDiamond: Boolean) : Field()
+enum class Field {
+    WALL,
+    PATH
 }
 
 
@@ -30,73 +30,71 @@ fun parseMap(serializedMap: String): SokobanMap {
     val header = lines[0]
     val map = lines.subList(1, lines.size)
     val (rowCount, colCount, diamondCount) = header.split(" ").map { it.toInt() }
-    val fields = map.map {
-        it.asSequence()
-                .map {
-                    when (it) {
-                        '.' -> Field.Path(isDiamondGoal = false, hasDiamond = false)
-                        'M' -> Field.Path(isDiamondGoal = false, hasDiamond = false)
-                        'G' -> Field.Path(isDiamondGoal = true, hasDiamond = false)
-                        'J' -> Field.Path(isDiamondGoal = false, hasDiamond = true)
-                        else -> Field.Wall
-                    }
-                }.toList()
-    }
-    val playerPosition = findPlayerPosition(map)
-    return SokobanMap(playerPosition, fields)
-}
+    val fields = mutableListOf<MutableList<Field>>()
+    val goals = mutableListOf<Location>()
+    val cans = mutableListOf<Location>()
+    var playerPosition = 0 to 0
 
-private fun findPlayerPosition(map: List<String>): Location {
     map.forEachIndexed { i, line ->
+        val lineList = mutableListOf<Field>()
         line.forEachIndexed { j, char ->
-            if (char == 'M') return i to j
+            val location = i to j
+            when (char) {
+                'M' -> playerPosition = location
+                'J' -> cans.add(location)
+                'G' -> goals.add(location)
+            }
+            lineList.add(if (char != 'X' && char != ' ') Field.PATH else Field.WALL)
         }
+        fields.add(lineList)
     }
-    throw IllegalArgumentException("Robot not found")
+
+    return SokobanMap(SokobanMap__Static(fields, goals), SokobanMap__Dynamic(playerPosition, cans))
 }
 
+
+fun isFinished(sokobanMap__Static: SokobanMap__Static, sokobanMap__Dynamic: SokobanMap__Dynamic) = sokobanMap__Static.goals == sokobanMap__Dynamic.canPositions
+
+
+val SokobanMap.isFinished
+    get() = isFinished(this.sokobanMap__Static, this.sokobanMap__Dynamic)
 
 data class SokobanMap(
-        private val playerPosition: Location,
-        private val fields: List<List<Field>>
+        val sokobanMap__Static: SokobanMap__Static,
+        val sokobanMap__Dynamic: SokobanMap__Dynamic
 ) {
 
-    operator fun get(i: Int, j: Int): Field = if (i < 0 || j < 0 || i >= fields.size || j >= fields[0].size) {
-        Field.Wall
-    } else {
-        fields[i][j]
-    }
-
-    val diamondGoals: List<Field.Path>
-        get() = fields.asSequence()
-                .flatMap { it.asSequence() }
-                .filter { it is Field.Path }
-                .map { it as Field.Path }
-                .filter { it.isDiamondGoal }
-                .toList()
-
-    val isFinished get() = diamondGoals.all { it.hasDiamond }
-
-
     override fun toString(): String = buildString {
-        fields.forEachIndexed { i, line ->
+        sokobanMap__Static.fields.forEachIndexed { i, line ->
             line.forEachIndexed { j, field ->
-                if (playerPosition.first == i && playerPosition.second == j) {
-                    append('M')
-                } else {
-                    if (field is Field.Path) {
-                        when {
-                            field.hasDiamond -> append('J')
-                            field.isDiamondGoal -> append('G')
-                            else -> append('.')
-                        }
-                    } else {
-                        append('X')
-                    }
+                val location = i to j
+                when (location) {
+                    sokobanMap__Dynamic.playerPosition -> append('M')
+                    in sokobanMap__Dynamic.canPositions -> append('J')
+                    in sokobanMap__Static.goals -> append('G')
+                    else -> if (field == Field.PATH) append('.') else append('X')
                 }
                 if (j == line.size - 1) append('\n')
             }
         }
     }
+}
+
+data class SokobanMap__Static(
+        val fields: List<List<Field>>,
+        val goals: List<Location>
+) {
+    operator fun get(i: Int, j: Int): Field = if (i < 0 || j < 0 || i >= fields.size || j >= fields[0].size) {
+        Field.WALL
+    } else {
+        fields[i][j]
+    }
+}
+
+data class SokobanMap__Dynamic(
+        val playerPosition: Location,
+        val canPositions: List<Location>
+
+) {
 
 }
