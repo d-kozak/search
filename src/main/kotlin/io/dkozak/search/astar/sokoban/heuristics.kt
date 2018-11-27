14819c71af84ad13d778d4e740b02a74781bf591
dkozak <dkozak94@gmail.com>
2018-11-27 12:26:37 +0100

    add dummy heuristics implementation

---
+++ src/main/kotlin/io/dkozak/search/astar/sokoban/heuristics.kt
@@ -0,0 +1,54 @@
+package io.dkozak.search.astar.sokoban
+
+import java.util.*
+import kotlin.collections.HashSet
+
+val SokobanMap.heuristic: Int
+    get() {
+        val canPositions = this.sokobanMap__Dynamic.canPositions
+        return canPositions.map { findNearestGoal(it, this) }
+                .map { it.size }
+                .sum()
+    }
+
+
+fun findNearestGoal(canPosition: Location, sokobanMap: SokobanMap): List<Location> {
+    data class Node(val location: Location, val parent: Node? = null)
+
+    val pathOf: (Node) -> List<Location> = {
+        var currentNode: Node? = it
+        val path = mutableListOf<Location>()
+        while (currentNode != null) {
+            path.add(currentNode.location)
+            currentNode = currentNode.parent
+        }
+        path
+    }
+
+    val getNeighbours: (Node) -> List<Node> = { node ->
+        val location = node.location
+        val neighbourIndexes = listOf(location.first - 1 to location.second,
+                location.first + 1 to location.second,
+                location.first to location.second - 1,
+                location.first to location.second + 1)
+        neighbourIndexes.filter { sokobanMap.sokobanMap__Static[it] == Field.PATH }
+                .map { Node(it, node) }
+
+    }
+
+    val openList = LinkedList<Node>()
+    openList.add(Node(canPosition))
+    val closedSed = HashSet<Node>()
+    while (openList.isNotEmpty()) {
+        val currentNode = openList.removeFirst()
+        if (currentNode.location in sokobanMap.sokobanMap__Static.goals)
+            return pathOf(currentNode)
+
+        if (currentNode in closedSed) continue
+
+        val neighbours = getNeighbours(currentNode)
+        openList.addAll(neighbours)
+
+    }
+    throw IllegalArgumentException("No path found")
+}
